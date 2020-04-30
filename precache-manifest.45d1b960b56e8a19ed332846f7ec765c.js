860b5aee512bb6f1ac3f88ae4ef065629d29c5e1
Hendri Faisal Hidayat <hendrifaisal18@gmail.com>
2020-04-30 19:23:42 +0700

    Updates

---
+++ precache-manifest.45d1b960b56e8a19ed332846f7ec765c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58cb977ccc8d05779a8937d71c44d02",
+    "url": "/hendri1.github.io/index.html"
+  },
+  {
+    "revision": "470e6348961683a05383",
+    "url": "/hendri1.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "89beb4a6437f8b3f1eaf",
+    "url": "/hendri1.github.io/static/js/2.373d03db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hendri1.github.io/static/js/2.373d03db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470e6348961683a05383",
+    "url": "/hendri1.github.io/static/js/main.b642ba9e.chunk.js"
+  },
+  {
+    "revision": "471bd1066b26294708db",
+    "url": "/hendri1.github.io/static/js/runtime-main.a1621693.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hendri1.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
